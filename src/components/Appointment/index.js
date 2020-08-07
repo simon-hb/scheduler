84d1527e63077d4d7659f658c66872f88287f201
simon-hb <simon.hb.jung@hotmail.com>
2020-08-06 20:20:26 -0400

    use visualMode helper function to display appointments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,21 +2,30 @@ import React from "react";
 import Header from "components/Appointment/Header";
 import Show from "components/Appointment/Show";
 import Empty from "components/Appointment/Empty";
+import Form from "components/Appointment/Form"
+import useVisualMode from "hooks/useVisualMode"
 import "components/Appointment/styles.scss";
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 export default function Appointment(props) {
+  const { mode, transition, back } = useVisualMode(
+    props.interview ? SHOW : EMPTY
+  );
   return (
     <article className="appointment">
       <Header time={props.time} />
 
-      {props.interview ? (
+      {mode === EMPTY &&  <Empty onAdd={() => transition('CREATE')} />}
+      {mode === SHOW && (
         <Show
           student={props.interview.student}
           interviewer={props.interview.interviewer.name}
         />
-      ) : (
-          <Empty />
-        )}
+      )}
+      {mode === CREATE && <Form onCancel={() => back()} interviewers = {[]} />}
     </article>
   );
 }
