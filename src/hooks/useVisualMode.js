06986ebcfbcd44103df73d8cd2411456fe94cce2
simon-hb <simon.hb.jung@hotmail.com>
2020-08-17 16:52:17 -0400

    fixed error handling

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,21 +6,29 @@ export default function useVisualMode(initial) {
 
   function transition(newMode, replace = false) {
     /* ... */
-    if(replace){
-      history.pop()
-      setHistory([...history, mode])
-    }else{
+    console.log('transition initial', history)
+    if (replace) {
+      const newHistory = history.slice(0, -1)
+      setHistory([...newHistory, newMode])
+      console.log('transition replace true', history)
+    } else {
       setHistory([...history, newMode])
+      console.log('transition replace false', history)
     }
     setMode(newMode)
+    console.log('transition default', history)
   }
 
   function back() {
-    //update this to not modify the history
-    if (history.length > 1) {
-      history.pop()
+    console.log('transition initial', history)
+    if (history.length <= 1) {
+      return
     }
-    setMode(history[history.length - 1]);
+    const newHistory = history.slice(0, -1)
+    setHistory(newHistory)
+    console.log('back history >1', history)
+    setMode(newHistory[newHistory.length - 1]);
+    console.log('back history default', history)
   }
 
   return { mode, transition, back };
